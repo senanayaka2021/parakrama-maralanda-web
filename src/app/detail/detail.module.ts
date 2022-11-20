16fc68860b411aff0af99853521e8bbc2c1a83d5
parakrama maralanda <parakrama.m@s2t.ai>
2022-11-20 09:08:08 +0530

    Changes for module routings

--- src/app/detail/detail.module.ts
+++ src/app/detail/detail.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { DetailRoutingModule } from './detail-routing.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    DetailRoutingModule
   ]
 })
 export class DetailModule { }
